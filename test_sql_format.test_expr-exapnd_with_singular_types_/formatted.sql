ecb9b46123bc6426616c928d7c50144b453cb737
Nikita Kozlovskiy <nikitka@gmail.com>
2023-01-13 15:07:18 +0100

    1673618838 5981c8

--- test_sql_format.test_expr-exapnd_with_singular_types_/formatted.sql
+++ test_sql_format.test_expr-exapnd_with_singular_types_/formatted.sql
@@ -1,6 +1,6 @@
 /* postgres can not */
-$null_t = Yql::NullType();
-$struct_t = Struct<a: String, b: List<Int32>, c: Dict<String, Int64>, d: EmptyList, e: EmptyDict, f: Void, g: $null_t, h: pgtext>;
+$null_t = TypeOf(NULL);
+$struct_t = Struct<a: String, b: Int32?, c: $null_t, d: pgtext>;
 $callable = CALLABLE (Callable<($struct_t) -> $struct_t>, ($x) -> {
     RETURN $x;
 });
