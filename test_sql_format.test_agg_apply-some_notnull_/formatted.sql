fcd6172542eb6b6dca7dab7cb667873256cf1cbd
Nikita Kozlovskiy <nikitka@gmail.com>
2023-01-17 18:59:05 +0100

    1673978345 db112d

---
+++ test_sql_format.test_agg_apply-some_notnull_/formatted.sql
@@ -0,0 +1,11 @@
+/* syntax version 1 *//* postgres can not */
+PRAGMA EmitAggApply;
+SELECT
+    some(key)
+FROM (
+    VALUES
+        (1)
+)
+    AS a (
+        key
+    );
---
+++ test_sql_format.test_agg_apply-some_null_/formatted.sql
@@ -0,0 +1,11 @@
+/* syntax version 1 *//* postgres can not */
+PRAGMA EmitAggApply;
+SELECT
+    some(key)
+FROM (
+    VALUES
+        (NULL)
+)
+    AS a (
+        key
+    );
