f5842ecacc0421b0ea4741a0347c9c8e4c5cd36b
Nikita Kozlovskiy <nikitka@gmail.com>
2023-02-09 13:43:53 +0100

    1675946633 ee15a5

---
+++ test_sql_format.test_blocks-top_sort_one_desc_/formatted.sql
@@ -0,0 +1,10 @@
+PRAGMA UseBlocks;
+USE plato;
+PRAGMA yt.UseNativeDescSort;
+SELECT
+    subkey + 0 AS subkey1,
+    value
+FROM Input
+ORDER BY
+    subkey1 DESC
+LIMIT 2;
---
+++ test_sql_format.test_blocks-top_sort_two_asc_/formatted.sql
@@ -0,0 +1,12 @@
+PRAGMA UseBlocks;
+USE plato;
+PRAGMA yt.UseNativeDescSort;
+SELECT
+    key,
+    subkey + 0 AS subkey1,
+    value
+FROM Input
+ORDER BY
+    key,
+    subkey1
+LIMIT 2;
