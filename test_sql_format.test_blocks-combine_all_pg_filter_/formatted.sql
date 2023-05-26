c17059514e982bea5632d0a3625b6ec8362020d7
Nikita Kozlovskiy <nikitka@gmail.com>
2023-05-26 22:40:06 +0200

    1685133606 851144

---
+++ test_sql_format.test_blocks-combine_all_pg_filter_/formatted.sql
@@ -0,0 +1,16 @@
+USE plato;
+PRAGMA EmitAggApply;
+PRAGMA UseBlocks;
+PRAGMA yt.UsePartitionsByKeysForFinalAgg = "false";
+SELECT
+    Pg::count(),
+    Pg::count(a),
+    Pg::sum(c),
+    Pg::min(a),
+    Pg::max(a),
+    Pg::min(c),
+    Pg::max(c),
+    Pg::avg(c),
+FROM Input
+WHERE d = "aaa";
+
